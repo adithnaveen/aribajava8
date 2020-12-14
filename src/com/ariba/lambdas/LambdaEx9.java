4cafb1ba51b8f73cfd26df5c9b4e4c736686a030
Naveen <adith.naveen@gmail.com>
2020-12-14 16:34:37 +0530

    Update LambdaEx9.java

--- src/com/ariba/lambdas/LambdaEx9.java
+++ src/com/ariba/lambdas/LambdaEx9.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+// program to generate infinite random numbers and with limits and generate random numbers with sorted order 
 public class LambdaEx9 {
 	public static void main(String[] args) {
 		
